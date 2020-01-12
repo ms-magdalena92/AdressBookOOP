d0d2b7823cd934fc031587236ec8d02f4059d4a2
Magda S <magdalena.slomiany.programista@gmail.com>
2020-01-12 11:07:15 +0100

    Add registered user to vector

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ using namespace std;
 int main()
 {
     KsiazkaAdresowa ksiazkaAdresowa;
+    ksiazkaAdresowa.rejestracjaUzytkownika();
+    ksiazkaAdresowa.rejestracjaUzytkownika();
+    ksiazkaAdresowa.rejestracjaUzytkownika();
+    ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
 
     return 0;
 }
