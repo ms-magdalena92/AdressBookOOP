c43a27ad2bc0d92f054977decce78d33d628d04e
Magda S <magdalena.slomiany.programista@gmail.com>
2020-01-16 17:35:55 +0100

    Create classes Adresat and AdresatMenedzer

---
+++ Adresat.h
@@ -0,0 +1,31 @@
+#ifndef ADRESAT_H
+#define ADRESAT_H
+
+#include <iostream>
+
+using namespace std;
+
+class Adresat
+{
+    int id;
+    int idUzytkownika;
+    string imie;
+    string nazwisko;
+    string numerTelefonu;
+    string email;
+    string adres;
+
+public:
+    void ustawId(int noweId);
+    void ustawImie(string noweImie);
+    void ustawNazwisko(string noweNazwisko);
+    void ustawNumerTelefonu(string nowyNumerTelefonu);
+    void ustawEmail(string nowyEmail);
+    void ustawAdres(string nowyAdres);
+
+    int pobierzId();
+    string pobierzImie();
+    string pobierzNazwisko();
+};
+
+#endif // ADRESAT_H
---
+++ AdresatMenedzer.h
@@ -0,0 +1,23 @@
+#ifndef ADRESATMENEDZER_H
+#define ADRESATMENEDZER_H
+
+#include <iostream>
+#include <vector>
+#include <windows.h>
+
+#include "Adresat.h"
+#include "Uzytkownik.h"
+
+using namespace std;
+
+class AdresatMenedzer
+{
+    int idOstatniegoAdresata;
+    vector<Adresat> adresaci;
+
+public:
+    //UzytkownikMenedzer(string nazwaPlikuZUzytkownikami) : plikZUzytkownikami(nazwaPlikuZUzytkownikami){};
+    void dodajAdresata();
+};
+
+#endif // ADRESATMENEDZER_H
